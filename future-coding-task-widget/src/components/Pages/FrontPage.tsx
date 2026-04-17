f74f7e0acef56886ef3cf67ad43345f46ba0442f
Warren Haslam <warren.haslam@gmail.com>
2026-04-17 13:01:51 +0100

    Created components, add shadcd Card, removed unused packages from init

---
+++ future-coding-task-widget/src/components/Pages/FrontPage.tsx
@@ -0,0 +1,25 @@
+import { cn } from "@/lib/utils"
+
+/*
+ * - - - - - - - - - - - - - - -
+ *
+ * Front page of the website.
+ *
+ * - - - - - - - - - - - - - - -
+ */
+export const FrontPage = () => {
+    console.debug('FrontPage RENDER')
+    return (
+        <div
+            data-id="FrontPage"
+            className={cn(
+                "flex flex-col content-center items-center h-full w-full",
+                "text-center overflow-y-auto bg-white text-gray-800 gap-[25px] pt-2 pb-10")} >
+            <div className="flex flex-col gap-2" >
+                <h1>Future Coding Task</h1>
+                <h2>Widget</h2>
+            </div>
+            <div>Hello World!</div>
+        </div>
+    )
+}
